04594d51d5ca887f045df4fda06755ca53915ea6
Alber <makaryanalber@gmail.com>
2024-04-17 00:21:38 -0700

    nnps dataset, rcnn and yolov7

---
+++ src/utils/convert.py
@@ -0,0 +1,16 @@
+"""
+The main data engineering tasks have been done using coco format dataset. 
+In this file, we are going to define functions to helps us to make a dataset appropriate for training different models.
+"""
+
+import os
+import shutil
+import json
+import yaml
+
+
+def convert_to_yolo(root,annotations_path):
+    
+    
+    pass
+
